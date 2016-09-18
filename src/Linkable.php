968480b20fa45c087e105bdcbf6f1f6771cc55b9
Tom Schlick <tom@runmybusiness.com>
2016-09-17 23:35:55 -0400

    source & composer.json

---
+++ src/Linkable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace TomSchlick\Linkable;
+
+/**
+ * Class Linkable
+ *
+ * @package TomSchlick\Linkable;
+ */
+trait Linkable
+{
+    /**
+     * Link directly to the resource
+     *
+     * @return string
+     */
+    public function link() : string
+    {
+        return $this->sublink('show');
+    }
+
+    /**
+     * Link to a sub-item of the resource
+     *
+     * @param string $key
+     * @param array $attr
+     *
+     * @return mixed
+     */
+    abstract public function sublink(string $key, array $attr = []) : string;
+
+    /**
+     * Redirect directly to the resource
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function redirect()
+    {
+        return redirect($this->link());
+    }
+
+    /**
+     * Redirect to a sub-item of the resource
+     *
+     * @param string $key
+     * @param array $attr
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function redirectSublink(string $key, array $attr = [])
+    {
+        return redirect($this->sublink($key, $attr));
+    }
+}
